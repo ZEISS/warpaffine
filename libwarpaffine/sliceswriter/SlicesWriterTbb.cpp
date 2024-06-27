6ea1c874c9bacf92530d4a325270eeee1c5f6439
Schwede, Robert <robert.schwede@zeiss.com>
2024-06-27 13:02:53 +0200

    suggestions from code review

--- libwarpaffine/sliceswriter/SlicesWriterTbb.cpp
+++ libwarpaffine/sliceswriter/SlicesWriterTbb.cpp
@@ -92,14 +92,14 @@ void CziSlicesWriterTbb::WriteWorker()
                     << std::setw(8) << std::setfill('0') << guid.Data1 << '-'
                     << std::setw(4) << std::setfill('0') << guid.Data2 << '-'
                     << std::setw(4) << std::setfill('0') << guid.Data3 << '-'
-                    << std::setw(2) << static_cast<int>(guid.Data4[0])
-                    << std::setw(2) << static_cast<int>(guid.Data4[1]) << '-'
-                    << std::setw(2) << static_cast<int>(guid.Data4[2])
-                    << std::setw(2) << static_cast<int>(guid.Data4[3])
-                    << std::setw(2) << static_cast<int>(guid.Data4[4])
-                    << std::setw(2) << static_cast<int>(guid.Data4[5])
-                    << std::setw(2) << static_cast<int>(guid.Data4[6])
-                    << std::setw(2) << static_cast<int>(guid.Data4[7])
+                    << std::setw(2) << static_cast<uint32_t>(guid.Data4[0])
+                    << std::setw(2) << static_cast<uint32_t>(guid.Data4[1]) << '-'
+                    << std::setw(2) << static_cast<uint32_t>(guid.Data4[2])
+                    << std::setw(2) << static_cast<uint32_t>(guid.Data4[3])
+                    << std::setw(2) << static_cast<uint32_t>(guid.Data4[4])
+                    << std::setw(2) << static_cast<uint32_t>(guid.Data4[5])
+                    << std::setw(2) << static_cast<uint32_t>(guid.Data4[6])
+                    << std::setw(2) << static_cast<uint32_t>(guid.Data4[7])
                     << std::dec
                     <<"</RetilingId></Tags></METADATA>";
 
